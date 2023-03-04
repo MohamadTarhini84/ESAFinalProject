6f5457d6f5f0d1d717d5a7fa1d55d06025375188
ibrahimmostapha <ibrahimmoutapha2000@gmail.com>
2023-03-04 14:15:52 +0200

    add-new-admin-component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Signup from './pages/signUp/signUp';
 import Signin from './pages/signIn/signIn';
 import EditProfile from './pages/editProfile/editProfile';
 
-import DashbordChannelSearch from './components/dashboardChannelSearch/DashbordChannelSearch';
+import AddNewAdmin from './components/addNewAdmin/AddNewAdmin';
 
 
 const App = () => {
@@ -46,7 +46,7 @@ const App = () => {
             <Route path="/editprofile" element={(<EditProfile />)} />
            
             {/* test  */}
-            <Route path="/test" element={(<DashbordChannelSearch />)} />
+            <Route path="/test" element={(<AddNewAdmin />)} />
 
             {/* dashboard 
             <Route path="/" element={(<Home />)} />
