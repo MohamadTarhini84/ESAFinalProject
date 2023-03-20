9315299550318af8a34b1353176fd7ac8a27c205
ibrahimmostapha <ibrahimmoutapha2000@gmail.com>
2023-03-20 22:26:02 +0200

    add-new-channel-done

--- client/src/components/addNewChannel/AddNewChannel.jsx
+++ client/src/components/addNewChannel/AddNewChannel.jsx
@@ -1,30 +1,59 @@
 import "./addNewChannel.css";
-
+import { useState } from 'react';
 
 function AddNewChannel() {
+  const [name, setName] = useState("");
+  const [logo, setLogo] = useState(null);
 
-    return (
-        <div className="add-new-channel-container">
-            <p className="add-channel-title">Add New Channel:</p>
+  function handleName(event) {
+    setName(event.target.value);
+  }
 
-            <div className="add-new-channel-inputs">
-                <div className="inputs-top-side">
-                    <div className="input_text flex-css">
-                        <input type="text" placeholder="Channel Name" />
-                    </div>
-                </div>
-                <div className="inputs-button-side">
-                    <div className="flex-css">
-                        <input class="custom-file-input" type="file" />
-                    </div>
-                    <div className="add-channel-btn flex-css">
-                        <button id="add-channel-button">Add</button>
-                    </div>
-                </div>
+  function handleLogo(event) {
+    setLogo(event.target.files[0]);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("image", logo);
+
+    fetch('http://localhost:3001/api/channels/new', {
+        method: "POST",
+        body: formData
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => {
+          console.error(error);
+          alert("There was an error adding the channel. Please try again later.");
+        });
+  }
+
+  return (
+    <div className="add-new-channel-container">
+      <p className="add-channel-title">Add New Channel:</p>
+
+      <form onSubmit={handleSubmit}>
+        <div className="add-new-channel-inputs">
+          <div className="inputs-top-side">
+            <div className="input_text flex-css">
+              <input type="text" placeholder="Channel Name" value={name} onChange={handleName} />
             </div>
-
+          </div>
+          <div className="inputs-button-side">
+            <div className="flex-css">
+              <input className="custom-file-input" type="file" onChange={handleLogo} />
+            </div>
+            <div className="add-channel-btn flex-css">
+              <button id="add-channel-button">Add</button>
+            </div>
+          </div>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
 export default AddNewChannel;
