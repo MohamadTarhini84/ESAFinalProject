6b734690a01fd73a7b6695ac56bd335e4f94b5fe
ibrahimmostapha <ibrahimmoutapha2000@gmail.com>
2023-03-28 12:48:47 +0300

    sideBar hot css tailwind

--- client/src/components/sideBar/SideBar.jsx
+++ client/src/components/sideBar/SideBar.jsx
@@ -1,39 +1,59 @@
 import "./sideBar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faBox, faTowerBroadcast, faTv, faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
 
 
 function SideBar(props) {
-
     return (
-        <div className="flex flex-col w-96 h-full bg-gray-200
+        <div className="flex flex-col w-72 h-full bg-gray-200
         sidebar-menu">
             <ul>
-                <li className="kabse active">
-                    <button onClick={() => props.function("dashboard")}>
+                <li
+                    className={`kabse bg-orange-900  ${props.state == "dashboard" ? "text-white" : "text-black"}`}
+                    onClick={() => props.function("dashboard")}
+                >
+                    <span><FontAwesomeIcon icon={faUsers}></FontAwesomeIcon></span>
+                    <button>
                         Dashboard
                     </button>
                 </li>
-                <li className="kabse">
-                    <button onClick={() => props.function("channels")}>
+                <li
+                    className={`kabse bg-gray-200 hover:bg-gray-300 ${props.state == "channels" ? "text-orange-900 bg-gray-300" : "text-black"}`}
+                    onClick={() => props.function("channels")} >
+                    <span><FontAwesomeIcon icon={faTv}></FontAwesomeIcon></span>
+                    <button>
                         Channels
                     </button>
                 </li>
-                <li className="kabse">
-                    <button onClick={() => props.function("broadcasts")}>
+                <li
+                    className={`kabse bg-gray-200 hover:bg-gray-300 ${props.state == "broadcasts" ? "text-orange-900 bg-gray-300" : "text-black"}`}
+                    onClick={() => props.function("broadcasts")}>
+                    <span><FontAwesomeIcon icon={faTowerBroadcast}></FontAwesomeIcon></span>
+                    <button>
                         Broadcasts
                     </button>
                 </li>
-                <li className="kabse">
-                    <button onClick={() => props.function("users")}>
+                <li
+                    className={`kabse bg-gray-200 hover:bg-gray-300 ${props.state == "users" ? "text-orange-900 bg-gray-300" : "text-black"}`}
+                    onClick={() => props.function("users")}>
+                    <span><FontAwesomeIcon icon={faUsers}></FontAwesomeIcon></span>
+                    <button>
                         Users
                     </button>
                 </li>
-                <li className="kabse">
-                    <button onClick={() => props.function("admins")}>
+                <li
+                    className={`kabse bg-gray-200 hover:bg-gray-300 ${props.state == "admins" ? "text-orange-900 bg-gray-300" : "text-black"}`}
+                    onClick={() => props.function("admins")}>
+                    <span><FontAwesomeIcon icon={faScrewdriverWrench}></FontAwesomeIcon></span>
+                    <button>
                         Admins
                     </button>
                 </li>
-                <li className="kabse">
-                    <button onClick={() => props.function("packages")}>
+                <li
+                    className={`kabse bg-gray-200 hover:bg-gray-300 ${props.state == "packages" ? "text-orange-900 bg-gray-300" : "text-black"}`}
+                    onClick={() => props.function("packages")}>
+                    <span><FontAwesomeIcon icon={faBox}></FontAwesomeIcon></span>
+                    <button>
                         Packages
                     </button>
                 </li>
